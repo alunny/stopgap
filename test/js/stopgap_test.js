a267ac8cd83976196974f45814d2efaf8b7478cf
alunny <alunny@gmail.com>
2011-03-10 16:20:16 -0800

    make sure PhoneGap gets defined

--- test/js/stopgap_test.js
+++ test/js/stopgap_test.js
@@ -1,14 +1,33 @@
-(function() {
+function bindOnce(target, name, fun, captures) {
+    var newFun = function () {
+        fun();
+        target.removeEventListener(name, newFun, captures);
+    }
+
+    target.addEventListener(name, newFun, captures);
+}
+
+(function(undef) {
 
     // mousedown
 
     module('deviceready');
 
     test('should fire a devicereadyevent', 1, function() {
-        document.addEventListener('deviceready', function () {
+        bindOnce(document, 'deviceready', function () {
             start();
             ok(true);
-        });
+        }, false);
+
+        stop();
+        StopGap();
+    });
+
+    test('at which point PhoneGap should be defined', 1, function () {
+        bindOnce(document, 'deviceready', function () {
+            start();
+            ok(window['PhoneGap']);
+        }, false);
 
         stop();
         StopGap();
